7cfd6d31d58130aa259108efad2511b6b1f309a1
REVE <aiwork4242@gmail.com>
2024-11-18 22:20:02 +0900

    build 경로 및 package install #1

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import GuideCard from '../components/GuideCard.vue';  // 위에서 만든 컴포넌트 임포트
-import MypageGuide from '../components/MypageGuide.vue'; 
-import GuideForm from '../components/GuideForm.vue'; 
-import GuideDetail from '../components/GuideDetail.vue'; 
+import GuideCard from '../components/GuideCard.vue'
+import MypageGuide from '../components/MypageGuide.vue'
+import GuideForm from '../components/GuideForm.vue'
+import GuideDetail from '../components/GuideDetail.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,31 +16,29 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue'),
     },
     {
       path: '/guide',
       name: 'Guide',
-      component: GuideCard,  // /guide 경로에 GuideCard 컴포넌트 렌더링
+      component: GuideCard,
     },
     {
       path: '/mypageguide',
       name: 'MypageGuide',
-      component: MypageGuide,  // /guide 경로에 GuideCard 컴포넌트 렌더링
+      component: MypageGuide,
     },
     {
-      path: '/guideform',
+      path: '/guideform/:id?', // 등록 및 수정 모두 처리
       name: 'GuideForm',
-      component: GuideForm,  // /guide 경로에 GuideCard 컴포넌트 렌더링
+      component: GuideForm,
+      props: true, // props로 ID 전달
     },
     {
-      //path: '/guide/:id',
-      path: '/guidedetail',
+      path: '/guidedetail/:id', // 상세보기 경로 수정
       name: 'GuideDetail',
-      component: GuideDetail,  // /guide 경로에 GuideCard 컴포넌트 렌더링
+      component: GuideDetail,
+      props: true, // 경로 매개변수를 props로 전달
     },
   ],
 })
