1d76409ab09dcef15d41517c12c57bca71ced47b
REVE <aiwork4242@gmail.com>
2024-11-19 20:46:50 +0900

    CORS 설정

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,19 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     vueDevTools(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     },
+//   },
+  
+// })
+
 export default defineConfig({
   plugins: [
     vue(),
@@ -12,7 +25,16 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // 백엔드 서버 주소
+        changeOrigin: true, // 백엔드 서버의 도메인을 변경
+        rewrite: (path) => path.replace(/^\/api/, ''), // '/api'를 제거
+      },
+    },
+  },
+});
