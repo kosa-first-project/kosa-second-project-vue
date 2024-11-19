54950af785e3d513e27d278c104c7cd99a4e3831
KimDoHun <thurr1394@gamil.com>
2024-11-19 15:13:20 +0900

    2024-11-19

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import Login from '../pages/Login.vue';
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/login', // 로그인 페이지 라우트
+      name: 'login',
+      component: Login, // Login 컴포넌트 사용
     },
     {
       path: '/about',
