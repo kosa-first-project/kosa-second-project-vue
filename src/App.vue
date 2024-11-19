438ac1bf8cd2e7f3861449a66bb704332edf0cb3
REVE <aiwork4242@gmail.com>
2024-11-19 14:34:31 +0900

    test app.vue 정렬 #4

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  
 <!--   
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
@@ -17,47 +18,19 @@ import HelloWorld from './components/HelloWorld.vue'
       </nav>
     </div>
   </header> -->
-  <div id="app">
     <div class="main-container">
       <RouterView />
-    </div>
   </div>
 </template>
 
 <style scoped>
-
-
-/* 브라우저 기본 스타일 초기화 */
-html, body {
-  margin: 0;
-  padding: 0;
-  width: 100%;
-  height: 100%;
+@media (min-width: 1024px) {
+  #app {
+      display: grid;
+      grid-template-columns: auto;
+      padding: 0 2rem;
+  }
 }
-
-/* 부모 요소가 반드시 100% 높이를 차지하도록 설정 */
-#app {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh; /* 화면 전체를 항상 차지하게 설정 */
-  width: 100vw; /* 화면 전체 넓이를 항상 차지하도록 설정 */
-}
-
-/* 자식 요소 중앙 정렬 */
-.main-container {
-  display: flex;
-  justify-content: center; /* 가로 중앙 */
-  align-items: center; /* 세로 중앙 */
-  text-align: center;
-  min-width: 300px; /* 최소 넓이 설정 */
-  min-height: 200px; /* 최소 높이 설정 */
-  padding: 20px;
-  box-sizing: border-box;
-  background-color: #f8f9fa; /* 배경색 추가 */
-}
-
-
 /* 
 header {
   line-height: 1.5;
