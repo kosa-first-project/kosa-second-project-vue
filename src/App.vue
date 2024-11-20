2e87e78a99d1fb245338669827f33fdb2f89d68c
REVE <aiwork4242@gmail.com>
2024-11-20 17:10:52 +0900

    헤더푸터 선택적용 #4

--- src/App.vue
+++ src/App.vue
@@ -1,105 +1,20 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import Navbar from '@/components/common/Navbar.vue';
-import Footer from '@/components/common/PageFooter.vue';
+import { RouterLink, RouterView, useRoute } from "vue-router";
+import Navbar from "@/components/HeaderSection.vue";
+import Footer from "@/components/FooterSection.vue";
+
+// 현재 경로 정보를 가져오기 위한 Vue Router composable
+const route = useRoute();
+
+// 헤더와 푸터를 숨길 라우트 이름 배열
+const noHeaderFooterRoutes = ["GuideDetail", "GuideForm"];
 </script>
 
 <template>
   <div class="main-container">
-    <Navbar /> <!-- 네비게이션 바 -->
-    <!-- <router-view /> --> <!-- 페이지 이동이 표시될 곳 -->
+    <!-- Navbar와 Footer를 특정 경로에서 제외 -->
+    <Navbar v-if="!noHeaderFooterRoutes.includes(route.name)" />
     <RouterView />
-    <Footer /> <!-- 푸터 컴포넌트 -->
+    <Footer v-if="!noHeaderFooterRoutes.includes(route.name)" />
   </div>
 </template>
-
-<!-- <style scoped>
-@media (min-width: 1024px) {
-  #app {
-      display: grid;
-      grid-template-columns: auto;
-  }
-}
-</style>
-<template>
-  <div id="app">
-    <router-view />
-  </div>
-</template> -->
-
-<script>
-export default {
-  name: "App",
-};
-</script>
-
-<!-- <style scoped>
-@media (min-width: 1024px) {
-  #app {
-      display: grid;
-      grid-template-columns: auto;
-  }
-}
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style> -->
